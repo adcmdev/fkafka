6101ab01794d87334fa63c55404a3989d0bcd9fb
Andres Carrillo <andres.carrillo@soytul.com>
2022-07-08 14:31:01 -0500

    Mvp

--- lib/flutter_kafka.dart
+++ lib/flutter_kafka.dart
@@ -1,7 +1,45 @@
 library flutter_kafka;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:uuid/uuid.dart';
+
+typedef OnTopicCallBack = void Function(Map<String, dynamic> data);
+
+class Kafka {
+  static List<KafkaSubscriber> _listeners = [];
+  final String uuid = const Uuid().v1();
+
+  /// Emit [topic] to all listeners
+  void emit(String topic, Map<String, dynamic> data) {
+    _listeners.where((k) => k.topic == topic).forEach((k) {
+      k.onTopic(data);
+    });
+  }
+
+  /// Listen come [topic]
+  void on(String topic, OnTopicCallBack onTopic) {
+    _listeners.add(
+      KafkaSubscriber(
+        uuid: uuid,
+        topic: topic,
+        onTopic: onTopic,
+      ),
+    );
+  }
+
+  /// Unsubscribe from topic [topic]
+  void unSubcribe() {
+    _listeners = _listeners.where((k) => k.uuid != uuid).toList();
+  }
+}
+
+class KafkaSubscriber {
+  final String uuid;
+  final String topic;
+  final OnTopicCallBack onTopic;
+
+  const KafkaSubscriber({
+    required this.uuid,
+    required this.topic,
+    required this.onTopic,
+  });
 }
