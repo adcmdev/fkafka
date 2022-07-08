995fc967ee7fd54525f5ec47d05552a6abfd04d9
Andres Carrillo <andres.carrillo@soytul.com>
2022-07-08 17:21:42 -0500

    Mvp

--- lib/flutter_kafka.dart
+++ lib/flutter_kafka.dart
@@ -6,10 +6,10 @@ typedef OnTopicCallBack = void Function(Map<String, dynamic> data);
 
 class Kafka {
   static List<KafkaSubscriber> _listeners = [];
-  final uuid = const Uuid().v1();
+  final _uuid = const Uuid().v1();
 
   /// Emit [topic] to all listeners
-  void emit(String topic, Map<String, dynamic> data) {
+  void emit(String topic, [Map<String, dynamic> data = const {}]) {
     _listeners.where((k) => k.topic == topic).forEach((k) {
       k.onTopic(data);
     });
@@ -19,7 +19,7 @@ class Kafka {
   void on(String topic, OnTopicCallBack onTopic) {
     _listeners.add(
       KafkaSubscriber(
-        uuid: uuid,
+        uuid: _uuid,
         topic: topic,
         onTopic: onTopic,
       ),
@@ -28,7 +28,7 @@ class Kafka {
 
   /// Unsubscribe from topic [topic]
   void unSubcribe() {
-    _listeners = _listeners.where((k) => k.uuid != uuid).toList();
+    _listeners = _listeners.where((k) => k.uuid != _uuid).toList();
   }
 }
 
