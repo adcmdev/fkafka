dead7953dfb8af16bbcb50b33309ee6b459d1ccd
limonadev <limonadev@gmail.com>
2022-10-04 12:27:15 -0500

    Replace sync listeners with async stream events

--- lib/fkafka.dart
+++ lib/fkafka.dart
@@ -1,66 +1,149 @@
 library fkafka;
 
-import 'package:uuid/uuid.dart';
+import 'dart:async';
 
-part 'package:fkafka/models/subscriber.dart';
+import 'package:equatable/equatable.dart';
+
+part 'package:fkafka/models/event.dart';
 part 'package:fkafka/models/producer.dart';
+part 'package:fkafka/models/subscriber.dart';
 part 'package:fkafka/models/topic.dart';
 
 typedef OnTopicCallBack = void Function(TopicData topic);
 
 class Fkafka {
-  static final List<FkafkaSubscriber> _listeners = [];
-  final _uuid = const Uuid().v1();
+  static final Map<String, StreamController<FkafkaEvent>> _controllers = {};
 
-  /// Emit [topic] to all listeners
+  final Map<String, List<FkafkaSubscriber>> _subscribers = {};
+
+  /// Should be called whenever Fkafka itself is not going to be
+  /// used anymore.
+  static void closeAll() {
+    for (final controller in _controllers.values) {
+      controller.close();
+    }
+    _controllers.clear();
+  }
+
+  /// Emit an event to all subscribers of [topic].
   void emit(String topic, [TopicData topicData = const TopicData()]) {
-    try {
-      _listeners.where((k) => k.topic == topic && k.active).forEach((k) {
-        final data = topicData.copyWith(
-          topic: topic,
-        );
+    _controllers.putIfAbsent(
+      topic,
+      () => StreamController.broadcast(),
+    );
 
-        k.onTopic(data);
-      });
-    } catch (_) {}
-  }
-
-  /// Listen come [topic]
-  void on(String topic, OnTopicCallBack onTopic) {
-    try {
-      _listeners.add(
-        FkafkaSubscriber(
-          uuid: _uuid,
+    _controllers[topic]!.add(
+      FkafkaEvent(
+        topic: topic,
+        topicData: topicData.copyWith(
           topic: topic,
-          onTopic: onTopic,
         ),
+      ),
+    );
+  }
+
+  /// Add a subscription to the [topic]
+  void listen({
+    required OnTopicCallBack onTopic,
+    required String topic,
+  }) {
+    _controllers.putIfAbsent(
+      topic,
+      () => StreamController.broadcast(),
+    );
+    _subscribers.putIfAbsent(
+      topic,
+      () => [],
+    );
+
+    final subscription = _controllers[topic]!.stream.listen(
+      (event) {
+        onTopic(
+          event.topicData,
+        );
+      },
+    );
+    _subscribers[topic]!.add(
+      FkafkaSubscriber(
+        isActive: true,
+        onTopic: onTopic,
+        subscription: subscription,
+      ),
+    );
+  }
+
+  /// Pause all the subscriptions from this instance of Fkafka to
+  /// the [topic].
+  void pauseListeningTo({
+    required String topic,
+  }) {
+    _controllers.putIfAbsent(
+      topic,
+      () => StreamController.broadcast(),
+    );
+
+    final subscribers = _subscribers[topic] ?? <FkafkaSubscriber>[];
+
+    for (var i = 0; i < subscribers.length; i++) {
+      final subscriber = subscribers[i];
+      subscriber.subscription.cancel();
+
+      subscribers[i] = subscriber.copyWith(
+        isActive: false,
       );
-    } catch (_) {}
+    }
   }
 
-  /// Close connection from topic [topic] listener
-  void close() {
-    if (index == -1) return;
+  /// Resume all the subscriptions from this instance of Fkafka to
+  /// the [topic].
+  void resumeListeningTo({
+    required String topic,
+  }) {
+    _controllers.putIfAbsent(
+      topic,
+      () => StreamController.broadcast(),
+    );
 
-    try {
-      _listeners[index] = _listeners[index].copyWith(active: false);
-    } catch (_) {}
+    final subscribers = _subscribers[topic] ?? <FkafkaSubscriber>[];
+
+    for (var i = 0; i < subscribers.length; i++) {
+      final subscriber = subscribers[i];
+      subscriber.subscription.cancel();
+
+      final subscription = _controllers[topic]!.stream.listen(
+        (event) {
+          subscriber.onTopic(
+            event.topicData,
+          );
+        },
+      );
+
+      subscribers[i] = subscriber.copyWith(
+        isActive: true,
+        subscription: subscription,
+      );
+    }
   }
 
-  /// ReOpen active connection, if connection is already closed from topic [topic] listener
-  void reOpen() {
-    if (index == -1) return;
-
-    try {
-      _listeners[index] = _listeners[index].copyWith(active: true);
-    } catch (_) {}
+  /// Check if there is at least one active subscription to the
+  /// [topic] in this instance of Fkafka.
+  bool isListeningTo({
+    required String topic,
+  }) {
+    return _subscribers[topic]?.isNotEmpty == true &&
+        _subscribers[topic]!.any(
+          (subscriber) => subscriber.isActive,
+        );
   }
 
-  bool get listening {
-    if (index == -1) return false;
-
-    return _listeners[index].active;
+  /// Should be called whenever the instance of this Fkafka object is
+  /// not going to be used anymore.
+  void closeInstance() {
+    for (final subscribers in _subscribers.values) {
+      for (final subscriber in subscribers) {
+        subscriber.subscription.cancel();
+      }
+    }
+    _subscribers.clear();
   }
-
-  int get index => _listeners.indexWhere((k) => k.uuid == _uuid);
 }
